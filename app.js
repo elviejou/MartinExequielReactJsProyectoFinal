85279e1774462fafdaf74655b9a87300334d42c0
elviejou <exequielcalo@gmail.com>
2022-10-15 16:02:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//3 maneras de crear un proyecto en react
+// 1(menos recomendado) 
+    //instalar npm install -g create-react-app
+    //create-react-app nombre-proyecto
+    //ingresar a la carpeta del proyecto
+    //npm start
+    //ctrl + c parar el servidor
+// 2 segunda forma
+    //1 crear una carpeta > npx create-creat-app nombre proyecto
+    //ingresar a la carpeta del proyecto
+    //npm start
+//3 (vite)
+//npm create vite@latest martin-exequiel-ecommerce-reactjs
+//npm i
+//npm run dev
+//ctrl + i sobre el link
+
+
+
